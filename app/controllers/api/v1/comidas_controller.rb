ef67abe5528ceab05b7edf06f98ec7802261d7ec
Javier Madrid <javierr.ma93@gmail.com>
2017-06-28 21:40:17 +0200

    Email comparasion

--- app/controllers/api/v1/comidas_controller.rb
+++ app/controllers/api/v1/comidas_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class ComidasController < ApplicationController
       def index
-        comidas = Comida.all
+        user = User.find_by(email_movil_app: params["email"])
+        comidas = user.comidas
         render json: comidas
       end
     end
--- db/migrate/20170511165132_create_comidas.rb
+++ db/migrate/20170511165132_create_comidas.rb
@@ -1,11 +1,11 @@
 class CreateComidas < ActiveRecord::Migration[5.1]
   def change
     create_table :comidas do |t|
-      t.string :nombre
-      t.float :peso
-      t.float :hidratosxracion
-      t.float :raciones
-      t.text :ingredientes
+      t.string :nombre, null: false, default: ""
+      t.float :peso, null: false
+      t.float :hidratosxracion, null: false
+      t.float :raciones, null: false
+      t.text :ingredientes, default: ""
 
       t.timestamps
     end
